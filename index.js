9a25f3f94b9fdd45b9d18ed9501d49978a423f90
ZonianMidian <57513632+ZonianMidian@users.noreply.github.com>
2025-06-07 04:22:18 -0500

    Prettier added

--- index.js
+++ index.js
@@ -8,178 +8,185 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
 async function getInfo(user) {
-    const { body, statusCode } = await got(`https://api.ivr.fi/v2/twitch/user?login=${user}`, {
-        throwHttpErrors: false,
-        responseType: 'json',
-        headers: { 'User-Agent': 'Emote Stats by ZonianMidian' },
-    });
-    if (statusCode < 200 || statusCode > 299 || !body.length) return null;
+	const { body, statusCode } = await got(`https://api.ivr.fi/v2/twitch/user?login=${user}`, {
+		throwHttpErrors: false,
+		responseType: 'json',
+		headers: { 'User-Agent': 'Emote Stats by ZonianMidian' }
+	});
+	if (statusCode < 200 || statusCode > 299 || !body.length) return null;
 
-    const displayName = body[0].displayName.toLowerCase() === user ? body[0].displayName : user;
-    return { name: displayName, avatar: body[0].logo, id: body[0].id };
+	const displayName = body[0].displayName.toLowerCase() === user ? body[0].displayName : user;
+	return { name: displayName, avatar: body[0].logo, id: body[0].id };
 }
 
 function emoteLink(id, extension) {
-    switch (extension) {
-        case 'BTTV':
-            return `https://cdn.betterttv.net/emote/${id}/2x`;
-        case 'FFZ':
-            return `https://cdn.frankerfacez.com/emoticon/${id}/2`;
-        case 'Twitch':
-            return `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/2.0`;
-    }
+	switch (extension) {
+		case 'BTTV':
+			return `https://cdn.betterttv.net/emote/${id}/2x`;
+		case 'FFZ':
+			return `https://cdn.frankerfacez.com/emoticon/${id}/2`;
+		case 'Twitch':
+			return `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/2.0`;
+	}
 }
 
 async function parseEmotes(emotes, extension) {
-    switch (extension) {
-        case 'BTTV':
-        case 'FFZ':
-        case 'Twitch': {
-            const emoteArray = [];
-            emotes.map((emote) => {
-                const emoteData = {
-                    name: emote.emote,
-                    id: emote.id,
-                    link: emoteLink(emote.id, extension),
-                    usage: emote.amount ?? 0,
-                };
-                emoteArray.push(emoteData);
-            });
-            return emoteArray;
-        }
-        case '7TV': {
-            const emoteArray = [];
-            emotes.map((emote) => {
-                const emoteData = {
-                    name: emote.name,
-                    alias: emote.alias,
-                    id: emote.id,
-                    link: `https://cdn.7tv.app/emote/${emote.id}/2x.webp`,
-                    usage: emote.count ?? 0,
-                };
-                if (emoteData.usage > 0) emoteArray.push(emoteData);
-            });
-            return emoteArray;
-        }
-    }
+	switch (extension) {
+		case 'BTTV':
+		case 'FFZ':
+		case 'Twitch': {
+			const emoteArray = [];
+			emotes.map((emote) => {
+				const emoteData = {
+					name: emote.emote,
+					id: emote.id,
+					link: emoteLink(emote.id, extension),
+					usage: emote.amount ?? 0
+				};
+				emoteArray.push(emoteData);
+			});
+			return emoteArray;
+		}
+		case '7TV': {
+			const emoteArray = [];
+			emotes.map((emote) => {
+				const emoteData = {
+					name: emote.name,
+					alias: emote.alias,
+					id: emote.id,
+					link: `https://cdn.7tv.app/emote/${emote.id}/2x.webp`,
+					usage: emote.count ?? 0
+				};
+				if (emoteData.usage > 0) emoteArray.push(emoteData);
+			});
+			return emoteArray;
+		}
+	}
 }
 
 async function getChannel(channel) {
-    const { body: SE_Stats, statusCode: SE_Status } = await got(
-        `https://api.streamelements.com/kappa/v2/chatstats/${channel}/stats`,
-        {
-            throwHttpErrors: false,
-            responseType: 'json',
-            headers: { 'User-Agent': 'Emote Stats by ZonianMidian' },
-        },
-    );
-    const SE =
-        SE_Status < 200 || SE_Status > 299
-            ? null
-            : {
-                  BTTV: await parseEmotes(SE_Stats.bttvEmotes, 'BTTV'),
-                  FFZ: await parseEmotes(SE_Stats.ffzEmotes, 'FFZ'),
-                  Twitch: await parseEmotes(SE_Stats.twitchEmotes, 'Twitch'),
-              };
+	const { body: SE_Stats, statusCode: SE_Status } = await got(
+		`https://api.streamelements.com/kappa/v2/chatstats/${channel}/stats`,
+		{
+			throwHttpErrors: false,
+			responseType: 'json',
+			headers: { 'User-Agent': 'Emote Stats by ZonianMidian' }
+		}
+	);
+	const SE =
+		SE_Status < 200 || SE_Status > 299
+			? null
+			: {
+					BTTV: await parseEmotes(SE_Stats.bttvEmotes, 'BTTV'),
+					FFZ: await parseEmotes(SE_Stats.ffzEmotes, 'FFZ'),
+					Twitch: await parseEmotes(SE_Stats.twitchEmotes, 'Twitch')
+				};
 
-    const { body: K_Stats, statusCode: K_Status } = await got(`https://7tv.markzynk.com/c/${channel}`, {
-        throwHttpErrors: false,
-        responseType: 'json',
-        headers: { 'User-Agent': 'Emote Stats by ZonianMidian' },
-    });
-    const K = K_Status < 200 || K_Status > 299 ? null : { STV: await parseEmotes(K_Stats.emotes, '7TV') };
+	const { body: K_Stats, statusCode: K_Status } = await got(
+		`https://7tv.markzynk.com/c/${channel}`,
+		{
+			throwHttpErrors: false,
+			responseType: 'json',
+			headers: { 'User-Agent': 'Emote Stats by ZonianMidian' }
+		}
+	);
+	const K =
+		K_Status < 200 || K_Status > 299 ? null : { STV: await parseEmotes(K_Stats.emotes, '7TV') };
 
-    if (!SE) return K;
-    if (!SE && !K) return null;
-    return Object.assign(SE, K);
+	if (!SE) return K;
+	if (!SE && !K) return null;
+	return Object.assign(SE, K);
 }
 
 async function getGlobals() {
-    const { body: SE_Stats } = await got(`https://api.streamelements.com/kappa/v2/chatstats/global/stats`, {
-        throwHttpErrors: false,
-        responseType: 'json',
-        headers: { 'User-Agent': 'Emote Stats by ZonianMidian' },
-    });
+	const { body: SE_Stats } = await got(
+		`https://api.streamelements.com/kappa/v2/chatstats/global/stats`,
+		{
+			throwHttpErrors: false,
+			responseType: 'json',
+			headers: { 'User-Agent': 'Emote Stats by ZonianMidian' }
+		}
+	);
 
-    const { body: K_Stats } = await got(`https://7tv.markzynk.com/top`, {
-        throwHttpErrors: false,
-        responseType: 'json',
-        headers: { 'User-Agent': 'Emote Stats by ZonianMidian' },
-    });
+	const { body: K_Stats } = await got(`https://7tv.markzynk.com/top`, {
+		throwHttpErrors: false,
+		responseType: 'json',
+		headers: { 'User-Agent': 'Emote Stats by ZonianMidian' }
+	});
 
-    return {
-        BTTV: await parseEmotes(SE_Stats.bttvEmotes, 'BTTV'),
-        FFZ: await parseEmotes(SE_Stats.ffzEmotes, 'FFZ'),
-        Twitch: await parseEmotes(SE_Stats.twitchEmotes, 'Twitch'),
-        STV: await parseEmotes(K_Stats.emotes, '7TV'),
-    };
+	return {
+		BTTV: await parseEmotes(SE_Stats.bttvEmotes, 'BTTV'),
+		FFZ: await parseEmotes(SE_Stats.ffzEmotes, 'FFZ'),
+		Twitch: await parseEmotes(SE_Stats.twitchEmotes, 'Twitch'),
+		STV: await parseEmotes(K_Stats.emotes, '7TV')
+	};
 }
 
 app.get('/', (req, res) => {
-    res.render('index');
+	res.render('index');
 });
 
 app.get('/global', async (req, res) => {
-    const globalData = await getGlobals();
-    res.render('global', { data: globalData });
+	const globalData = await getGlobals();
+	res.render('global', { data: globalData });
 });
 
 app.get('/faq', (req, res) => {
-    res.render('faq');
+	res.render('faq');
 });
 
 app.get('/contact', async (req, res) => {
-    const userInfo = await getInfo('zonianmidian');
-    res.render('contact', userInfo);
+	const userInfo = await getInfo('zonianmidian');
+	res.render('contact', userInfo);
 });
 
 app.get('/channel', (req, res) => {
-    res.redirect('/global');
+	res.redirect('/global');
 });
 
 app.get('/c', (req, res) => {
-    res.redirect('/global');
+	res.redirect('/global');
 });
 
 app.get('/channel/:name', (req, res) => {
-    res.redirect(`/c/${req.params.name}`);
+	res.redirect(`/c/${req.params.name}`);
 });
 
 app.get('/c/:name', async (req, res) => {
-    const user = req.params.name.toLowerCase();
+	const user = req.params.name.toLowerCase();
 
-    if (user === 'global') {
-        const globalData = await getGlobals();
-        res.render('global', { data: globalData });
-    } else {
-        if (!new RegExp(/^[a-z0-9]\w{0,24}$/i).exec(user))
-            return res.render('error', { error: 'Invalid username', code: '' });
+	if (user === 'global') {
+		const globalData = await getGlobals();
+		res.render('global', { data: globalData });
+	} else {
+		if (!new RegExp(/^[a-z0-9]\w{0,24}$/i).exec(user))
+			return res.render('error', { error: 'Invalid username', code: '' });
 
-        const userInfo = await getInfo(user);
-        if (!userInfo) return res.render('error', { error: 'User not found', code: '' });
+		const userInfo = await getInfo(user);
+		if (!userInfo) return res.render('error', { error: 'User not found', code: '' });
 
-        const channelData = await getChannel(user);
-        if (!channelData)
-            return res.render('error', {
-                error: 'No statistics available',
-                code: '',
-            });
+		const channelData = await getChannel(user);
+		if (!channelData)
+			return res.render('error', {
+				error: 'No statistics available',
+				code: ''
+			});
 
-        res.render('channel', { data: channelData, user: userInfo });
-    }
+		res.render('channel', { data: channelData, user: userInfo });
+	}
 });
 
 app.use(function (req, res, next) {
-    const err = new Error('Not found');
-    err.status = 404;
-    next(err);
+	const err = new Error('Not found');
+	err.status = 404;
+	next(err);
 });
 
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', { error: err.message, code: `${err.status} - ` });
+	res.status(err.status || 500);
+	res.render('error', { error: err.message, code: `${err.status} - ` });
 });
 
 app.listen(port, () => {
-    console.log(`Statistics website listening on ${port}`);
+	console.log(`Statistics website listening on ${port}`);
 });
--- static/index.js
+++ static/index.js
@@ -1,93 +1,93 @@
 function UpdateEmote(data, type) {
-    const { count = 0, amount, name, alias, key, id } = data;
-    const emoteCount = document.getElementById(`${type}-${id}`);
+	const { count = 0, amount, name, alias, key, id } = data;
+	const emoteCount = document.getElementById(`${type}-${id}`);
 
-    if (emoteCount) {
-        const oldCount = parseInt(emoteCount.innerHTML.replace(/\.|'|,/g, ''), 10);
-        const newCount = oldCount + (count || amount);
-        emoteCount.innerHTML = newCount.toLocaleString('en-US');
+	if (emoteCount) {
+		const oldCount = parseInt(emoteCount.innerHTML.replace(/\.|'|,/g, ''), 10);
+		const newCount = oldCount + (count || amount);
+		emoteCount.innerHTML = newCount.toLocaleString('en-US');
 
-        const emoteDiv = document.getElementById(`emote-${type}-${id}`);
-        emoteDiv.dataset.count = newCount;
+		const emoteDiv = document.getElementById(`emote-${type}-${id}`);
+		emoteDiv.dataset.count = newCount;
 
-        reorderDivs(type);
-    } else {
-        const emoteDiv = document.getElementById(`${type}-emotes`);
-        const divs = emoteDiv.querySelectorAll('div');
-        const nextNumber = divs.length / 4;
+		reorderDivs(type);
+	} else {
+		const emoteDiv = document.getElementById(`${type}-emotes`);
+		const divs = emoteDiv.querySelectorAll('div');
+		const nextNumber = divs.length / 4;
 
-        const emoteRow = document.createElement('div');
-        emoteRow.className = `row ${type}`;
-        emoteRow.id = `emote-${type}-${id}`;
-        emoteRow.dataset.count = amount || count;
-        emoteDiv.appendChild(emoteRow);
+		const emoteRow = document.createElement('div');
+		emoteRow.className = `row ${type}`;
+		emoteRow.id = `emote-${type}-${id}`;
+		emoteRow.dataset.count = amount || count;
+		emoteDiv.appendChild(emoteRow);
 
-        const emoteCount = document.createElement('div');
-        emoteCount.className = 'col-2 flex-center countNumber';
-        emoteCount.textContent = nextNumber;
-        emoteRow.appendChild(emoteCount);
+		const emoteCount = document.createElement('div');
+		emoteCount.className = 'col-2 flex-center countNumber';
+		emoteCount.textContent = nextNumber;
+		emoteRow.appendChild(emoteCount);
 
-        const emoteImage = document.createElement('div');
-        emoteImage.className = 'col-5 flex-center';
-        emoteRow.appendChild(emoteImage);
+		const emoteImage = document.createElement('div');
+		emoteImage.className = 'col-5 flex-center';
+		emoteRow.appendChild(emoteImage);
 
-        const emoteImageTag = document.createElement('img');
-        emoteImageTag.className = 'emote_image';
-        emoteImageTag.src = emoteLink(id, type);
-        emoteImageTag.title = key || alias || name;
-        emoteImageTag.alt = key || alias || name;
-        emoteImageTag.draggable = false;
-        emoteImageTag.onerror = () => {
-            emoteImageTag.src = '/static/unavailable.png';
-        };
-        emoteImage.appendChild(emoteImageTag);
+		const emoteImageTag = document.createElement('img');
+		emoteImageTag.className = 'emote_image';
+		emoteImageTag.src = emoteLink(id, type);
+		emoteImageTag.title = key || alias || name;
+		emoteImageTag.alt = key || alias || name;
+		emoteImageTag.draggable = false;
+		emoteImageTag.onerror = () => {
+			emoteImageTag.src = '/static/unavailable.png';
+		};
+		emoteImage.appendChild(emoteImageTag);
 
-        const counterNumber = document.createElement('div');
-        counterNumber.className = 'col-5';
-        emoteRow.appendChild(counterNumber);
+		const counterNumber = document.createElement('div');
+		counterNumber.className = 'col-5';
+		emoteRow.appendChild(counterNumber);
 
-        const emoteName = document.createElement('h3');
-        emoteName.className = 'emote_name';
-        emoteName.textContent = key || alias || name;
-        counterNumber.appendChild(emoteName);
+		const emoteName = document.createElement('h3');
+		emoteName.className = 'emote_name';
+		emoteName.textContent = key || alias || name;
+		counterNumber.appendChild(emoteName);
 
-        const emoteUsage = document.createElement('p');
-        emoteUsage.className = 'emote_usage';
-        emoteUsage.id = `${type}-${id}`;
-        emoteUsage.textContent = amount || count;
-        counterNumber.appendChild(emoteUsage);
+		const emoteUsage = document.createElement('p');
+		emoteUsage.className = 'emote_usage';
+		emoteUsage.id = `${type}-${id}`;
+		emoteUsage.textContent = amount || count;
+		counterNumber.appendChild(emoteUsage);
 
-        reorderDivs(type);
-    }
+		reorderDivs(type);
+	}
 }
 
 function emoteLink(id, extension) {
-    switch (extension) {
-        case 'bttv':
-            return `https://cdn.betterttv.net/emote/${id}/2x`;
-        case 'ffz':
-            return `https://cdn.frankerfacez.com/emoticon/${id}/2`;
-        case 'twitch':
-            return `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/2.0`;
-        case '7tv':
-            return `https://cdn.7tv.app/emote/${id}/2x.webp`;
-    }
+	switch (extension) {
+		case 'bttv':
+			return `https://cdn.betterttv.net/emote/${id}/2x`;
+		case 'ffz':
+			return `https://cdn.frankerfacez.com/emoticon/${id}/2`;
+		case 'twitch':
+			return `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/2.0`;
+		case '7tv':
+			return `https://cdn.7tv.app/emote/${id}/2x.webp`;
+	}
 }
 
 function reorderDivs(type) {
-    let divList = document.querySelectorAll(`.${type}`);
-    divList = Array.from(divList).sort((a, b) => b.dataset.count - a.dataset.count);
+	let divList = document.querySelectorAll(`.${type}`);
+	divList = Array.from(divList).sort((a, b) => b.dataset.count - a.dataset.count);
 
-    const emoteDiv = document.getElementById(`${type}-emotes`);
-    emoteDiv.innerHTML = '';
+	const emoteDiv = document.getElementById(`${type}-emotes`);
+	emoteDiv.innerHTML = '';
 
-    let i = 1;
-    divList.forEach((div) => {
-        if (div.className === `row ${type}`) {
-            const countNumber = div.querySelector('.countNumber');
-            countNumber.textContent = i;
-            emoteDiv.appendChild(div);
-            i++;
-        }
-    });
+	let i = 1;
+	divList.forEach((div) => {
+		if (div.className === `row ${type}`) {
+			const countNumber = div.querySelector('.countNumber');
+			countNumber.textContent = i;
+			emoteDiv.appendChild(div);
+			i++;
+		}
+	});
 }
